74e87c71ffae53035db6c6a5720d974f9df1c094
THEProgrammingGOD69 <arnav.sirse@gmail.com>
2025-05-04 17:49:12 +0530

    Updated website content and removed leadership profile

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "jarvees-infotech.firebaseapp.com",
-  projectId: "jarvees-infotech",
-  storageBucket: "jarvees-infotech.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyDLphx-Cj2Z-Gk2i0vxgfrZAo6_L91HlgE",
+  authDomain: "jarveesinfotech-89913.firebaseapp.com",
+  projectId: "jarveesinfotech-89913",
+  storageBucket: "jarveesinfotech-89913.firebasestorage.app",
+  messagingSenderId: "114156250125",
+  appId: "1:114156250125:web:eb872a39f48c3bfe61a144",
+  measurementId: "G-PK7FWCJWKD"
 };
 
 // Initialize Firebase
